3829ff68fb150080dfc0109d029fb1c59c36b15c
Alan Tai <ayltai@gmail.com>
2017-02-11 17:19:42 +0800

    Integrate CI

---
+++ scripts/ci-helpers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function generateTestReports() {
+    ./gradlew -i testDebugUnitTest jacocoTestReport uploadCoverageToCodacy --console=plain
+    cp -r adapters/build/test-results/testDebugUnitTest/debug/*.xml $CIRCLE_TEST_REPORTS && cp -r adapters/build/reports/tests/testDebugUnitTest $CIRCLE_TEST_REPORTS && cp -r adapters/build/reports/jacoco/jacocoTestReport $CIRCLE_TEST_REPORTS
+}
+
+function inspectCode() {
+    ./gradlew -i testDebugUnitTest adapters:customFindBugs --console=plain
+    cp -r adapters/build/outputs/findbugs $CIRCLE_TEST_REPORTS
+}
